7efe229ec7b88131b18e43977b63fb8f7f10430d
Luca <shaohsiung@163.com>
2021-03-22 20:59:24 +0800

    refactor hero.py

--- hero.py
+++ hero.py
@@ -1,19 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+ 
+HERO_URL_PREFIX = 'https://ow.blizzard.cn/heroes/'
+ 
 
-#todo
-HERO_URL = 'https://ow.blizzard.cn/heroes/doomfist'
-
-res = requests.get(HERO_URL)
-status = res.status_code
-if status == 200:
-    result = dict()
-    soup = BeautifulSoup(res.content, 'html.parser')
-    hero_name = soup.find_all("h1", class_="hero-name")[0].text
-    result['hero_name'] = hero_name
-    # 技能
+def get_hero_name(soup):
+    return soup.find_all("h1", class_="hero-name")[0].text
+ 
+ 
+def get_hero_abilities(soup):
+    result = []
     hero_abilities = soup.find("ul", class_="ability-button-list").find_all('li')
-    abilities = []
     for hero_ability in hero_abilities:
        ability = dict()
        hero_ability_name = hero_ability['data-ability-name'].strip()
@@ -23,56 +20,105 @@ if status == 200:
            ability['title'] = '英雄模型'
        else:
            ability['title'] = hero_ability_name
-       abilities.append(ability)
-
+       result.append(ability)
+ 
     # 视频链接
     ability_showcases = soup.find_all('div', class_="ability-showcase")[0].find_all('video')
     i = 0
     for showcase in ability_showcases:
         video_url = showcase.find_all('source')[0]['src']
-        abilities[i]['video_url'] = video_url
+        result[i]['video_url'] = video_url
         i = i + 1
-    result['ability'] = abilities
-
-    print(result)
-    
-    # overview
+    return result
+ 
+ 
+def get_hero_overview(soup):
+    overview = dict()
     role_type = soup.find_all('h4', class_="h2 hero-detail-role-name")[0].text
-    print(role_type)
+    overview['role_type'] = role_type
+ 
     difficulty = soup.find_all('div', class_="hero-detail-difficulty")[0].find_all('span')
-    print(len(difficulty))
+    overview['difficulty'] = len(difficulty)
+ 
     hero_detail_description = soup.find_all('p', class_="hero-detail-description")[0].text
-    print(hero_detail_description)
-    hero_detail_abilities = soup.find_all('div', class_="hero-detail-wrapper m-same-pad")[0].find_all('div', class_='hero-ability')
-    for hero_detail_ability in hero_detail_abilities:
-        ability_descriptor = hero_detail_ability.find('div', class_='hero-ability-descriptor')
-        ability_name = ability_descriptor.find_all('h4', class_='h5')[0].text
-        ability_detail = ability_descriptor.find_all('p')[0].text
-        print(ability_name)
-        print(ability_detail)
-
-    # story
+    overview['detail_description'] = hero_detail_description
+ 
+    ability_details_model = list()
+    hero_ability_details = soup.find_all('div', class_="hero-detail-wrapper m-same-pad")[0].find_all('div', class_='hero-ability')
+    for hero_ability_detail in hero_ability_details:
+        ability_detail_model = dict()
+        ability_descriptor = hero_ability_detail.find('div', class_='hero-ability-descriptor')
+        ability_name = hero_ability_detail.find_all('h4', class_='h5')[0].text
+        ability_detail = hero_ability_detail.find_all('p')[0].text
+        ability_detail_model['ability_name'] = ability_name
+        ability_detail_model['ability_detail'] = ability_detail
+        ability_details_model.append(ability_detail_model)
+    overview['ability_details'] = ability_details_model
+    return overview
+ 
+ 
+def get_hero_bio(soup):
+    """
+    全称 职业 行动基地 隶属
+    name occupation base affiliation
+    """
+    result = list()
+    bio_titles = ['affilaiation', 'base', 'occupation', 'name']
     hero_bio = soup.find_all('ul', class_='hero-bio')[0].find_all('li')
-    '''
-    todo s
-    name
-    occupation
-    base
-    affiliation
-    '''
     for item in hero_bio:
+        bio_model = dict()
         bio = item.find('span', class_='hero-bio-copy').text
-        print(bio)
-
-    # 口头蝉
-    detail_title = soup.find('p', class_='h4 hero-detail-title').text
-    print(detail_title)
-
-    # 背景故事
+        bio_title = bio_titles.pop()
+        # TODO(luca): 正则分割：后字符串
+        bio_model[bio_title] = bio
+        result.append(bio_model)
+    return result
+    
+ 
+ 
+def get_hero_detail_title(soup):
+    """
+    口头禅
+    """
+    return soup.find('p', class_='h4 hero-detail-title').text
+ 
+ 
+def get_hero_bio_backstory(soup):
+    """
+    背景故事
+    """
     bio_backstory = soup.find('div', class_='hero-bio-backstory pad-sm').find_all('p')
     backstory_res = ''
     for item in bio_backstory:
         backstory_res = backstory_res + item.text + '</br>'
-    print(backstory_res)
-    
-       
+    return backstory_res
+ 
+ 
+"""
+???
+"""
+def get_hero(hero_name):
+    # todo hero_name
+    hero_url = HERO_URL_PREFIX + hero_name.strip()
+    res = requests.get(hero_url)
+    status = res.status_code
+    if status == 200:
+        hero_model = dict()
+        soup = BeautifulSoup(res.content, 'html.parser')
+ 
+        hero_model['hero_name'] = get_hero_name(soup)
+        hero_model['ability'] = get_hero_abilities(soup)
+        hero_model['overview'] = get_hero_overview(soup)
+        hero_model['bio'] = get_hero_bio(soup)
+        hero_model['detail_title'] = get_hero_detail_title(soup)
+        hero_model['bio_backstory'] = get_hero_bio_backstory(soup)
+ 
+        print(hero_model)
+ 
+ 
+"""
+改造为对象
+"""
+if __name__ == '__main__':
+    hero_name = 'doomfist'
+    get_hero(hero_name)
